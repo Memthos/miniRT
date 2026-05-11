6bcf5e60f34308c28243b601b3fc7d9c4eb302b5
Memthos <perrinetrebi.m@gmail.com>
2026-05-11 13:57:13 +0200

    remove old macrolibx

--- includes/minirt.h
+++ includes/minirt.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minirt.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mperrine <mperrine@student.42angouleme.fr  +#+  +:+       +#+        */
+/*   By: mperrine <mperrine@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/10 22:53:00 by mperrine          #+#    #+#             */
-/*   Updated: 2026/05/10 22:55:55 by mperrine         ###   ########.fr       */
+/*   Updated: 2026/05/11 13:54:17 by mperrine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,13 @@
 
 # include <math.h>
 # include <limits.h>
+# include <sys/time.h> // gettimeofday
+# include <sys/stat.h> // open
+# include <fcntl.h> // open
+# include <unistd.h> // close, read, write
+# include <stdio.h> // printf, perror
+# include <stdlib.h> // malloc, free, exit
+# include <string.h> // strerror
 
 # include "vectors.h"
 # include "types.h"
