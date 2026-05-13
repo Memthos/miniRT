bf7a6a791ec0c02fb1807dcc125527656ec92145
Memthos <perrinetrebi.m@gmail.com>
2026-05-13 15:03:02 +0200

    style: norme color_conv

--- srcs/utils/color_conversions.c
+++ srcs/utils/color_conversions.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   color_conversion.c                                 :+:      :+:    :+:   */
+/*   color_conversions.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mperrine <mperrine@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/12 10:17:26 by mperrine          #+#    #+#             */
-/*   Updated: 2026/05/12 11:01:00 by mperrine         ###   ########.fr       */
+/*   Updated: 2026/05/13 13:29:21 by mperrine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,12 @@
 uint32_t	parse_color(t_uint channels[4], t_status *status)
 {
 	uint32_t	color;
+
 	if (channels[0] > 255 || channels[1] > 255
 		|| channels[2] > 255 || channels[3] > 255)
 	{
 		if (status)
-			*status = COLOR_RANGE;
+			*status = COLOR_OVERFLOW;
 		return (0);
 	}
 	color = channels[0];
