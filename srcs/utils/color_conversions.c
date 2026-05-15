fe6861220011dc6f8381ab5ec13c69d8a9e744e6
Memthos <perrinetrebi.m@gmail.com>
2026-05-15 13:59:44 +0200

    use global status in color conversion

--- srcs/utils/color_conversions.c
+++ srcs/utils/color_conversions.c
@@ -6,21 +6,20 @@
 /*   By: mperrine <mperrine@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/12 10:17:26 by mperrine          #+#    #+#             */
-/*   Updated: 2026/05/14 17:12:46 by mperrine         ###   ########.fr       */
+/*   Updated: 2026/05/15 13:26:06 by mperrine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
-uint32_t	parse_color(t_uint channels[4], t_status *status)
+uint32_t	parse_color(t_uint channels[4])
 {
 	uint32_t	color;
 
 	if (channels[0] > 255 || channels[1] > 255
 		|| channels[2] > 255 || channels[3] > 255)
 	{
-		if (status)
-			*status = OVERFLOW;
+		use_status(OVERFLOW);
 		return (0);
 	}
 	color = channels[0];
