ee97239ed901fc5fe4547019d64bf051d17cecf3
Mathis <perrinetrebi.m@gmail.com>
2026-05-15 22:43:14 +0200

    feat: Start objects parsing

---
+++ srcs/parser/parser.c
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mperrine <mperrine@student.42angouleme.fr  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/15 21:53:49 by mperrine          #+#    #+#             */
+/*   Updated: 2026/05/15 22:42:11 by mperrine         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minirt.h"
+
+static t_obj_type	get_object(t_string line)
+{
+	const t_string	data[] = {"sp", "pl", "cy", "A", "L", "C"};
+	size_t			i;
+
+	i = 0;
+	while (data[i])
+	{
+		if (ft_strncmp(data[i], line, ft_strlen(data[i])) == SUCCESS
+			&& ft_isspace(line[ft_strlen(data[i]) + 1]) == SUCCESS)
+			return (i);
+		i++;
+	}
+	return (-1);
+}
+
+void	parse_geo(t_string arg, t_obj_type type, t_minirt *minirt)
+{
+	t_string_tab	coords;
+	size_t			i;
+
+	if (use_status(ERR_GET) != SUCCESS)
+		return ;
+	coords = ft_split(arg, ",");
+	if (use_status(ERR_GET) != SUCCESS)
+		return ;
+	i = 0;
+	while (coords[i])
+}
+
+void	parse_line(t_string line, t_minirt *minirt)
+{
+	size_t			i;
+	t_string_tab	args;
+	t_obj_type		type;
+
+	type = get_object(line);
+	if ((int)type == -1)
+	{
+		write(2, "miniRT: Wrong object in map\n", 28);
+		use_status(FAILURE);
+		return ;
+	}
+	args = ft_split(line, "\t\n\v\f\r ");
+	if (use_status(ERR_GET) != SUCCESS)
+		return ;
+	i = 1;
+	while (args[i++])
+	{
+		if (type == SPHERE || type == PLANE || type == CYLINDER)
+			parse_geo(args[i - 1], type, minirt);
+		else if (type == AMBIENT_LIGHT || type == POINT_LIGHT)
+			parse_light(args[i - 1], type, minirt);
+		else
+		 	parse_camera(args[i - 1], type, minirt);
+	}
+	ft_free_tab(&args);
+}
