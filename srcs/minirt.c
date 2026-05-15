78012d40cf5a7b6647f0e62a15f5acc03c6d3849
Memthos <perrinetrebi.m@gmail.com>
2026-05-15 16:00:56 +0200

    Add check for file existence and empty file

--- srcs/minirt.c
+++ srcs/minirt.c
@@ -6,7 +6,7 @@
 /*   By: mperrine <mperrine@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/11 14:17:41 by mperrine          #+#    #+#             */
-/*   Updated: 2026/05/15 13:36:39 by mperrine         ###   ########.fr       */
+/*   Updated: 2026/05/15 15:59:39 by mperrine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,29 @@
 static int	check_filename(t_string s)
 {
 	size_t	len;
+	int		fd;
+	char	c;
 
 	len = ft_strlen(s);
 	if (len < 4 || ft_strcmp(&s[len - 3], ".rt") != 0)
 		return (FAILURE);
-	return (SUCCESS);
+	fd = open(s, O_RDONLY) == -1;
+	if (fd == -1)
+	{
+		use_status(OPEN_FAILURE);
+		return (FAILURE);
+	}
+	if (read(fd, &c, 1) == -1)
+		use_status(READ_FAILURE);
+	close(fd);
+	if (use_status(ERR_GET) != SUCCESS)
+		return (FAILURE);
+	if (!c)
+	{
+		use_status(EMPTY_FILE);
+		return (FAILURE);
+	}
+	return (use_status(SUCCESS));
 }
 
 // static void	camera_init(t_minirt *minirt)
@@ -52,9 +70,13 @@ int	main(int argc, t_string_tab argv)
 {
 	t_minirt	minirt;
 
-	if (argc != 2 || check_filename(argv[1]))
+	if (argc != 2 || check_file(argv[1]))
 	{
-		write(2, "Use: ./miniRT <file.rt>\n", 24);
+		write(2 ,"Error\n", 6);
+		if (use_status(ERR_GET) != SUCCESS)
+			print_status("miniRT");
+		else
+			write(2, "Use: ./miniRT <file.rt>\n", 24);
 		return (1);
 	}
 	minirt = global_init();
