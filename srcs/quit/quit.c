057012d7bab858b9b42b9148efea73a22da9e7a5
julienPR86 <julien.pr86@gmail.com>
2026-05-19 15:50:02 +0200

    feat : added rt_quit function

---
+++ srcs/quit/quit.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   quit.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/19 15:21:54 by juperrin          #+#    #+#             */
+/*   Updated: 2026/05/19 15:21:54 by juperrin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minirt.h"
+
+void	rt_quit(t_minirt *rt)
+{
+	if (NULL == rt)
+		return ;
+	mlx_destroy_image(rt->context, rt->render);
+	mlx_destroy_window(rt->context, rt->window);
+	mlx_destroy_context(rt->context);
+	return ;
+}
