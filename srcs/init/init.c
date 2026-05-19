8fe741bf0c5e09902fa83e4adf7b643c5309baca
julienPR86 <julien.pr86@gmail.com>
2026-05-19 15:48:18 +0200

    feat : added rt_init function

---
+++ srcs/init/init.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/19 15:24:04 by juperrin          #+#    #+#             */
+/*   Updated: 2026/05/19 15:26:58 by juperrin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minirt.h"
+
+t_minirt	rt_init(void)
+{
+	t_minirt				rt;
+	mlx_window_create_info	info;
+
+	ft_bzero(&rt, sizeof(t_minirt));
+	rt.aspect_ratio = 16.0 / 9.0;
+	rt.dimensions = (t_vec2){1280.0, 1280.0 / rt.aspect_ratio};
+	rt.context = mlx_init();
+	ft_bzero(&info, sizeof(info));
+	info.title = "miniRT";
+	info.width = rt.dimensions.x;
+	info.height = rt.dimensions.y;
+	rt.window = mlx_new_window(rt.context, &info);
+	rt.render = mlx_new_image(rt.context, rt.dimensions.x, rt.dimensions.y);
+	rt_init_events(&rt);
+	return (rt);
+}
