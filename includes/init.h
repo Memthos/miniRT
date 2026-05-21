d060bca9956ebe39e9fa7c5eb2a04c904fcacb2c
julienPR86 <julien.pr86@gmail.com>
2026-05-21 15:07:57 +0200

    fix : modified init function to not reset minirt structure after parsing

--- includes/init.h
+++ includes/init.h
@@ -6,7 +6,7 @@
 /*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/19 15:24:38 by juperrin          #+#    #+#             */
-/*   Updated: 2026/05/19 15:25:39 by juperrin         ###   ########.fr       */
+/*   Updated: 2026/05/21 15:00:42 by juperrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,6 @@ typedef struct s_minirt	t_minirt;
 /**
  * @brief Initialises a minirt structure and return it.
  */
-t_minirt	rt_init(void);
+t_minirt	*rt_init(t_minirt *rt);
 
 #endif
--- srcs/init/init.c
+++ srcs/init/init.c
@@ -6,27 +6,26 @@
 /*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/19 15:24:04 by juperrin          #+#    #+#             */
-/*   Updated: 2026/05/21 14:12:21 by juperrin         ###   ########.fr       */
+/*   Updated: 2026/05/21 15:00:54 by juperrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
-t_minirt	rt_init(void)
+t_minirt	*rt_init(t_minirt *rt)
 {
-	t_minirt				rt;
 	mlx_window_create_info	info;
 
-	rt.aspect_ratio = 16.0 / 9.0;
-	rt.dimensions = (t_vec2){1280, 1280 / rt.aspect_ratio};
-	rt.context = mlx_init();
+	rt->aspect_ratio = 16.0 / 9.0;
+	rt->dimensions = (t_vec2){1280, 1280 / rt->aspect_ratio};
+	rt->context = mlx_init();
 	ft_bzero(&info, sizeof(info));
 	info.title = "miniRT";
-	info.width = rt.dimensions.x;
-	info.height = rt.dimensions.y;
-	rt.window = mlx_new_window(rt.context, &info);
-	rt.render = mlx_new_image(rt.context, rt.dimensions.x, rt.dimensions.y);
-	rt.camera = camera_init(60, rt.dimensions);
-	rt_init_events(&rt);
+	info.width = rt->dimensions.x;
+	info.height = rt->dimensions.y;
+	rt->window = mlx_new_window(rt->context, &info);
+	rt->render = mlx_new_image(rt->context, rt->dimensions.x, rt->dimensions.y);
+	camera_init(&rt->camera, rt->dimensions);
+	rt_init_events(rt);
 	return (rt);
 }
