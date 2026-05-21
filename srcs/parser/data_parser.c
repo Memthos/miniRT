d92c45f1263493b9022f3432db578e60482bd05b
Memthos <perrinetrebi.m@gmail.com>
2026-05-21 13:12:49 +0200

    Add check for clamping values in parsing

--- srcs/parser/data_parser.c
+++ srcs/parser/data_parser.c
@@ -6,7 +6,7 @@
 /*   By: mperrine <mperrine@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/19 13:52:14 by mperrine          #+#    #+#             */
-/*   Updated: 2026/05/20 16:03:18 by mperrine         ###   ########.fr       */
+/*   Updated: 2026/05/21 13:12:22 by mperrine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,17 +34,28 @@ void	rt_parse_orientation(const t_string *input, t_vec3 *norm_rot)
 {
 	t_string_tab	data;
 	t_string		endptr;
+	double			value;
+	size_t			i;
 
 	endptr = NULL;
 	data = ft_split(*input, ",");
 	if (ft_string_tab_len(data) != 3)
 		use_status(FAILURE);
-	if (use_status(ERR_GET) == SUCCESS)
-		norm_rot->x = ft_strtod(data[0], &endptr);
-	if (use_status(ERR_GET) == SUCCESS && endptr == NULL)
-		norm_rot->y = ft_strtod(data[1], &endptr);
-	if (use_status(ERR_GET) == SUCCESS && endptr == NULL)
-		norm_rot->z = ft_strtod(data[2], &endptr);
+	i = 0;
+	while (i < 3 && use_status(ERR_GET) == SUCCESS)
+	{
+		value = ft_strtod(data[i++], &endptr);
+		if (use_status(ERR_GET) != SUCCESS)
+			break ;
+		else if (value < -1.0 || value > 1.0 || endptr)
+			use_status(FAILURE);
+		else if (i == 1)
+			norm_rot->x = value;
+		else if (i == 2)
+			norm_rot->y = value;
+		else
+			norm_rot->z = value;
+	}
 	ft_free_tab(&data);
 }
 
@@ -59,20 +70,19 @@ void	rt_parse_color(const t_string *input, mlx_color *color)
 		use_status(FAILURE);
 	color->a = 0xFF;
 	i = 0;
-	while (i < 3)
+	while (i < 3 && use_status(ERR_GET) == SUCCESS)
 	{
 		value = ft_atoi(data[i++]);
-		if (use_status(ERR_GET) == SUCCESS && value >= 0 && value <= 255)
-		{
-			if (i == 1)
-				color->r = value;
-			else if (i == 2)
-				color->g = value;
-			else
-				color->b = value;
-		}
-		else
+		if (use_status(ERR_GET) != SUCCESS)
 			break ;
+		else if (value < 0 || value > 255)
+			use_status(FAILURE);
+		else if (i == 1)
+			color->r = value;
+		else if (i == 2)
+			color->g = value;
+		else
+			color->b = value;
 	}
 	ft_free_tab(&data);
 }
