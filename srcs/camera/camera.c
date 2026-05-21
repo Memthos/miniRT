37bb66a2ca71101b51201a0d42612819a61c2663
julienPR86 <julien.pr86@gmail.com>
2026-05-21 11:19:55 +0200

    feat : added camera initialisation

--- srcs/camera/camera.c
+++ srcs/camera/camera.c
@@ -6,20 +6,26 @@
 /*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/19 17:26:13 by juperrin          #+#    #+#             */
-/*   Updated: 2026/05/21 10:43:10 by juperrin         ###   ########.fr       */
+/*   Updated: 2026/05/21 11:17:30 by juperrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
-t_camera	camera_init(double fov)
+t_camera	camera_init(double fov, t_vec2 screen)
 {
 	t_camera	camera;
 
 	ft_bzero(&camera, sizeof(camera));
-	camera.height = 2.0;
-	camera.width = camera.height * (16.0 / 9.0);
 	camera.focal_length = 1.0;
 	camera.fov = fov;
+	camera.viewport.height = 2.0;
+	camera.viewport.width = camera.viewport.height * (16.0 / 9.0);
+	camera.viewport.delta_x = (t_vec3){camera.viewport.width / screen.x, 0, 0};
+	camera.viewport.delta_y = (t_vec3){0, -camera.viewport.height / screen.y, 0};
+	camera.viewport.nw_corner = vec_sub(camera.pos, (t_vec3){0, 0, camera.focal_length});
+	camera.viewport.nw_corner = vec_sub(camera.viewport.nw_corner, vec_scale((t_vec3){camera.viewport.width, 0, 0}, 0.5));
+	camera.viewport.nw_corner = vec_sub(camera.viewport.nw_corner, vec_scale((t_vec3){0, -camera.viewport.height, 0}, 0.5));
+	camera.viewport.nw_pixel = vec_add(camera.viewport.nw_corner, vec_scale(vec_add(camera.viewport.delta_x, camera.viewport.delta_y), 0.5));
 	return (camera);
 }
