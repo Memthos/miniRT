5c1670037ea963c73f8b845fbe5d2f10e8878db3
julienPR86 <julien.pr86@gmail.com>
2026-05-26 13:21:07 +0200

    feat : created hit_point structure to store informations when a ray hits an object

--- includes/ray.h
+++ includes/ray.h
@@ -6,7 +6,7 @@
 /*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/19 16:32:40 by juperrin          #+#    #+#             */
-/*   Updated: 2026/05/20 12:53:57 by juperrin         ###   ########.fr       */
+/*   Updated: 2026/05/26 13:17:30 by juperrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,14 @@
 
 typedef struct s_vec3	t_point3;
 
+typedef struct s_hit_point
+{
+	t_point3	point;
+	t_vec3		normal;
+	double		t;
+	bool		front;
+}	t_hit_point;
+
 typedef struct s_ray
 {
 	t_point3	origin;
@@ -26,7 +34,5 @@ typedef struct s_ray
 t_point3	ray_at(t_ray *ray, double delta);
 
 mlx_color	ray_color(t_minirt *rt, t_ray *ray);
-
-void	display_vec(t_vec3	*vec);
 
 #endif
