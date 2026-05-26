5fc8b1d03302cef833843f0a77d924c6b4812269
julienPR86 <julien.pr86@gmail.com>
2026-05-26 15:42:37 +0200

    feat : created interval_clamp function

--- srcs/interval/interval.c
+++ srcs/interval/interval.c
@@ -6,7 +6,7 @@
 /*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/26 14:45:26 by juperrin          #+#    #+#             */
-/*   Updated: 2026/05/26 15:07:49 by juperrin         ###   ########.fr       */
+/*   Updated: 2026/05/26 15:41:27 by juperrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,4 +25,13 @@ bool	interval_contains(t_interval *inter, double x)
 bool	interval_surrounds(t_interval *inter, double x)
 {
 	return (x > inter->min && x < inter->max);
+}
+
+double	interval_clamp(t_interval *inter, double x)
+{
+	if (x < inter->min)
+		return (inter->min);
+	if (x > inter->max)
+		return (inter->max);
+	return (x);
 }
