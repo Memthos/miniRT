f4e707c6b4512e81677ace7e05ee345997e0569b
julienPR86 <julien.pr86@gmail.com>
2026-05-26 15:42:22 +0200

    feat : added interval_clamp function declaration

--- includes/interval.h
+++ includes/interval.h
@@ -6,7 +6,7 @@
 /*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/26 14:45:40 by juperrin          #+#    #+#             */
-/*   Updated: 2026/05/26 15:07:54 by juperrin         ###   ########.fr       */
+/*   Updated: 2026/05/26 15:41:49 by juperrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,5 +24,7 @@ double	interval_size(t_interval *inter);
 bool	interval_contains(t_interval *inter, double x);
 
 bool	interval_surrounds(t_interval *inter, double x);
+
+double	interval_clamp(t_interval *inter, double x);
 
 #endif
