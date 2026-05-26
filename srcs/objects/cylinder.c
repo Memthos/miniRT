6abaf74f29151a8b30c4239114e4e94e0766a248
julienPR86 <julien.pr86@gmail.com>
2026-05-26 13:49:38 +0200

    fix : updated object function prototypes

--- srcs/objects/cylinder.c
+++ srcs/objects/cylinder.c
@@ -6,13 +6,13 @@
 /*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/26 11:20:52 by juperrin          #+#    #+#             */
-/*   Updated: 2026/05/26 13:07:45 by juperrin         ###   ########.fr       */
+/*   Updated: 2026/05/26 13:44:20 by juperrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
-bool	hit_cylinder(t_minirt *rt, t_ray *ray, t_cylinder *cy, double min, double max, t_hit_point *p)
+bool	hit_cylinder(t_minirt *rt, t_ray *ray, t_obj *cy, double min, double max, t_hit_point *p)
 {
 	(void)rt;
 	(void)ray;
--- srcs/objects/sphere.c
+++ srcs/objects/sphere.c
@@ -6,19 +6,19 @@
 /*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/26 11:20:46 by juperrin          #+#    #+#             */
-/*   Updated: 2026/05/26 13:17:46 by juperrin         ###   ########.fr       */
+/*   Updated: 2026/05/26 13:43:35 by juperrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
-bool	hit_sphere(t_minirt *rt, t_ray *ray, t_sphere *sp, double min, double max, t_hit_point *p)
+bool	hit_sphere(t_minirt *rt, t_ray *ray, t_obj *obj, double min, double max, t_hit_point *p)
 {
 	(void)rt;
-	t_vec3	oc = vec_sub(sp->position, ray->origin);//vector between camera origin and sphere center
+	t_vec3	oc = vec_sub(obj->u_obj.sphere.position, ray->origin);//vector between camera origin and sphere center
 	double	a = vec_squared_magnitude(ray->dir);
 	double	h = vec_dot(ray->dir, oc);
-	double	c = vec_squared_magnitude(oc) - sp->diameter * sp->diameter * 0.25;
+	double	c = vec_squared_magnitude(oc) - obj->u_obj.sphere.diameter * obj->u_obj.sphere.diameter * 0.25;
 	double	d = h * h - a *c;
 	if (d < 0)
 		return (false);
@@ -32,7 +32,7 @@ bool	hit_sphere(t_minirt *rt, t_ray *ray, t_sphere *sp, double min, double max, 
 	}
 	p->t = root;
 	p->point = ray_at(ray, p->t);
-	t_vec3	normal = vec_scale(vec_sub(p->point, sp->position), 1 / (sp->diameter / 2));
+	t_vec3	normal = vec_scale(vec_sub(p->point, obj->u_obj.sphere.position), 1 / (obj->u_obj.sphere.diameter / 2));
 	const bool	front = vec_dot(ray->dir, normal) < 0;
 	if (front)
 		p->normal = normal;
