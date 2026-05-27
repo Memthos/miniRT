d757c77185e4e180bac03b5ac39af75986eb98a1
julienPR86 <julien.pr86@gmail.com>
2026-05-27 16:21:16 +0200

    feat : fov is now working with the vertical fov

--- srcs/camera/camera.c
+++ srcs/camera/camera.c
@@ -6,7 +6,7 @@
 /*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/19 17:26:13 by juperrin          #+#    #+#             */
-/*   Updated: 2026/05/27 15:56:01 by juperrin         ###   ########.fr       */
+/*   Updated: 2026/05/27 16:09:07 by juperrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ t_camera	*camera_init(t_camera *cam, t_vec2 screen)
 	cam->aa.size = 2;
 	cam->aa.scale = 1.0 / (double)(cam->aa.size * cam->aa.size);
 	cam->focal_length = 1.0;
-	cam->viewport.height = 2.0;
+	cam->viewport.height = 2.0 * tan(cam->vfov * deg_to_rad() / 2) * cam->focal_length;
 	cam->viewport.width = cam->viewport.height * (16.0 / 9.0);
 	cam->viewport.delta_x = (t_vec3){cam->viewport.width / screen.x, 0, 0};
 	cam->viewport.delta_y = (t_vec3){0, -cam->viewport.height / screen.y, 0};
