936e5f3b39614a7c119e5b9a5f530648aba40aeb
julienPR86 <julien.pr86@gmail.com>
2026-06-01 17:57:03 +0200

    feat : created a quality struct to store information for different levels of rendering

--- includes/rendering.h
+++ includes/rendering.h
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   rendering.c                                        :+:      :+:    :+:   */
+/*   rendering.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/19 16:05:03 by juperrin          #+#    #+#             */
-/*   Updated: 2026/05/19 16:05:51 by juperrin         ###   ########.fr       */
+/*   Updated: 2026/06/01 16:56:58 by juperrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,13 @@
 
 typedef struct s_minirt	t_minirt;
 
-void	rt_render(t_minirt *rt);
+typedef struct s_render_quality
+{
+	double	quality;
+	t_vec2	region;
+	t_aa	aa;
+}	t_quality;
+
+void	rt_render(t_minirt *rt, bool restart);
 
 #endif
