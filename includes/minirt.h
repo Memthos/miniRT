2dd07d22cb289a541e52f6be0e43186a334a6fbf
julienPR86 <julien.pr86@gmail.com>
2026-06-02 11:18:02 +0200

    feat : added mouse_state field to minirt struct

--- includes/minirt.h
+++ includes/minirt.h
@@ -6,7 +6,7 @@
 /*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/10 22:53:00 by mperrine          #+#    #+#             */
-/*   Updated: 2026/06/01 17:47:34 by juperrin         ###   ########.fr       */
+/*   Updated: 2026/06/02 11:17:38 by juperrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,23 +34,25 @@
 # include "objects.h"
 # include "rendering.h"
 # include "events.h"
+# include "mouse.h"
 # include "random.h"
 
 typedef struct s_minirt
 {
-	bool		should_render;
-	double		aspect_ratio;
-	t_vec2		dimensions;
-	t_quality	max_quality;
-	t_quality	min_quality;
-	t_quality	*cur_quality;
-	mlx_context	context;
-	mlx_window	window;
-	mlx_image	render;
-	t_array		geos;
-	t_array		lights;
-	t_camera	camera;
-	t_obj		*selected;
+	bool			should_render;
+	double			aspect_ratio;
+	t_vec2			dimensions;
+	t_mouse_state	mouse;
+	t_quality		max_quality;
+	t_quality		min_quality;
+	t_quality		*cur_quality;
+	mlx_context		context;
+	mlx_window		window;
+	mlx_image		render;
+	t_array			geos;
+	t_array			lights;
+	t_camera		camera;
+	t_obj			*selected;
 }	t_minirt;
 
 void	rt_loop(void *param);
