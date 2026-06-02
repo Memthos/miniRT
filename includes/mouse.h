f8d205d90e828c462375489069b8ebf25e93c7a0
julienPR86 <julien.pr86@gmail.com>
2026-06-02 14:06:21 +0200

    feat : created mouse_init func

--- includes/mouse.h
+++ includes/mouse.h
@@ -6,7 +6,7 @@
 /*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/02 10:21:37 by juperrin          #+#    #+#             */
-/*   Updated: 2026/06/02 11:16:44 by juperrin         ###   ########.fr       */
+/*   Updated: 2026/06/02 11:26:29 by juperrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,8 @@ typedef struct s_mouse_state
 	bool	rotating;
 	t_vec2	oldpos;
 }	t_mouse_state;
+
+void	mouse_init(t_minirt *rt);
 
 t_vec2	mouse_get_delta(t_minirt *rt);
 
--- srcs/mouse/mouse.c
+++ srcs/mouse/mouse.c
@@ -6,11 +6,23 @@
 /*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/02 10:34:23 by juperrin          #+#    #+#             */
-/*   Updated: 2026/06/02 11:14:13 by juperrin         ###   ########.fr       */
+/*   Updated: 2026/06/02 11:26:17 by juperrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
+
+void	mouse_init(t_minirt *rt)
+{
+	int	x;
+	int	y;
+
+	mlx_mouse_get_pos(rt->context, &x, &y);
+	rt->mouse.oldpos.x = x;
+	rt->mouse.oldpos.y = y;
+	rt->mouse.rotating = false;
+	return ;
+}
 
 t_vec2	mouse_get_delta(t_minirt *rt)
 {
