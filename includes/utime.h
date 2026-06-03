a1197f78214e9b5f60fbe28f2a99720e4404d293
JulienPR <julien.pr86@gmail.com>
2026-06-03 12:19:15 +0200

    # renamed timee to utime

--- includes/utime.h
+++ includes/utime.h
@@ -1,18 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   timee.h                                            :+:      :+:    :+:   */
+/*   utime.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/03 10:47:57 by juperrin          #+#    #+#             */
-/*   Updated: 2026/06/03 12:01:00 by juperrin         ###   ########.fr       */
+/*   Updated: 2026/06/03 12:16:50 by juperrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef TIMEE_H
-# define TIMEE_H
+#ifndef UTIME_H
+# define UTIME_H
 
-time_t	get_utime(void);
+typedef unsigned long	t_utime;
+
+t_utime	get_utime(void);
+
+t_utime	get_delta_time(void);
 
 #endif
