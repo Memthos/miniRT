0069d6bfae73231a0bca53e48116a555b08eb501
JulienPR <julien.pr86@gmail.com>
2026-06-03 12:21:18 +0200

    # renamed time to utime

--- srcs/utime/utime.c
+++ srcs/utime/utime.c
@@ -6,17 +6,29 @@
 /*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/03 10:49:10 by juperrin          #+#    #+#             */
-/*   Updated: 2026/06/03 12:00:55 by juperrin         ###   ########.fr       */
+/*   Updated: 2026/06/03 12:07:25 by juperrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
-time_t	get_utime(void)
+t_utime	get_utime(void)
 {
 	struct timeval	t;
 
 	if (SUCCESS != gettimeofday(&t, NULL))
 		return (0);
-	return (t.tv_sec * 1000000 + t.tv_usec);		
+	return ((t_utime)t.tv_sec * 1000000 + t.tv_usec);		
+}
+
+t_utime	get_delta_time(void)
+{
+	static t_utime	last = 0;
+	t_utime			cur;
+	t_utime			delta;
+
+	cur = get_utime();
+	delta = cur - last;
+	last = cur;
+	return (delta);
 }
