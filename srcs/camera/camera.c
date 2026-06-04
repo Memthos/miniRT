c5250922378400d4a1b782dd3fdcbd5296071b71
julienPR86 <julien.pr86@gmail.com>
2026-06-04 16:09:45 +0200

    feat : camera position is now updated based on velicity and its direction

--- srcs/camera/camera.c
+++ srcs/camera/camera.c
@@ -6,7 +6,7 @@
 /*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/19 17:26:13 by juperrin          #+#    #+#             */
-/*   Updated: 2026/06/04 10:46:25 by juperrin         ###   ########.fr       */
+/*   Updated: 2026/06/04 16:08:22 by juperrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,11 @@ t_camera	*camera_init(t_camera *cam, t_aa aa, const t_vec2 screen)
 t_camera	*camera_update(t_camera *cam, t_aa aa, t_utime delta, const t_vec2 screen)
 {
 	const t_vec3	vup = {0, 1, 0};
+	const t_vec3	vel = vec_add(
+			vec_scale(cam->dir, cam->move.velocity.z),
+			vec_add(
+				vec_scale(cam->right, cam->move.velocity.x),
+				vec_scale(vup, cam->move.velocity.y)));
 	t_vec3			vx;
 	t_vec3			vy;
 
@@ -32,9 +37,8 @@ t_camera	*camera_update(t_camera *cam, t_aa aa, t_utime delta, const t_vec2 scre
 	cam->up = vec_cross(cam->dir, cam->right);
 	vx = vec_scale(cam->right, cam->viewport.width);
 	vy = vec_scale(cam->up, -cam->viewport.height);
-
 	cam->aa = aa;
-	cam->pos = vec_add(cam->pos, vec_scale(cam->move.velocity, cam->move.speed * (delta * 0.000001)));
+	cam->pos = vec_add(cam->pos, vec_scale(vel, cam->move.speed * (delta * 0.000001)));
 	cam->viewport.delta_x = vec_scale(vx, 1.0 / screen.x);
 	cam->viewport.delta_y =  vec_scale(vy, 1.0 / screen.y);
 	cam->viewport.nw_corner = vec_add(cam->pos, cam->dir);
