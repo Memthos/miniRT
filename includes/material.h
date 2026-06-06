d29c7d6a0fcd7fbbb72a5d983f0c838a0473d9ea
julienPR86 <julien.pr86@gmail.com>
2026-06-06 15:56:29 +0200

    feat : created material structure to store information about obj material

---
+++ includes/material.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   material.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/06 14:33:42 by juperrin          #+#    #+#             */
+/*   Updated: 2026/06/06 15:21:23 by juperrin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MATERIAL_H
+# define MATERIAL_H
+
+# include "../libs/lib_tools/includes/math_ops.h"
+
+
+typedef struct s_ray		t_ray;
+typedef struct s_hit_point	t_hit_point;
+typedef struct s_vec3		t_rgb;
+typedef bool	(*t_scatter_func)(t_ray *in, t_ray *out, t_hit_point *p);
+
+typedef struct s_material
+{
+	t_rgb			col;
+	t_scatter_func	scatter;
+}	t_material;
+
+t_material	mat_lambertian(t_rgb col);
+
+#endif
