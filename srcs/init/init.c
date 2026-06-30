dff50e5391d364a5b8e287b44d7059c18f47242b
julienPR86 <julien.pr86@gmail.com>
2026-06-30 17:18:20 +0200

    - removed set_quality from init source file

--- srcs/init/init.c
+++ srcs/init/init.c
@@ -6,33 +6,11 @@
 /*   By: juperrin <juperrin@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/19 15:24:04 by juperrin          #+#    #+#             */
-/*   Updated: 2026/06/30 16:49:16 by juperrin         ###   ########.fr       */
+/*   Updated: 2026/06/30 17:13:34 by juperrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
-
-static void	set_quality(t_minirt *rt)
-{
-	const float	fact = 10;
-
-	rt->max_quality = (t_quality){
-		.ray_depth = 5,
-		.quality = 1,
-		.region = (t_vec2){rt->dimensions.x / fact, rt->dimensions.y / fact},
-		.aa.size = 4,
-	};
-	rt->max_quality.aa.scale = 1 / (double)(
-			rt->max_quality.aa.size * rt->max_quality.aa.size);
-	rt->min_quality = (t_quality){
-		.ray_depth = 2,
-		.quality = 0.1,
-		.region = rt->dimensions,
-		.aa = (t_aa){1, 1}
-	};
-	rt->cur_quality = &rt->max_quality;
-	return ;
-}
 
 t_minirt	*rt_init(t_minirt *rt)
 {
